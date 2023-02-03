fc274da5f17305ae7469fb4c3f50a0072d601217
MlamliLolwane <mrlolwane96@gmail.com>
2023-02-03 23:06:26 +0200

    Created a new axios instance with axios http verbs

---
+++ src/axiosClient.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+const axiosClient = axios.create();
+
+axiosClient.defaults.baseURL = 'http://localhost:8000/api';
+axiosClient.defaults.headers = {
+    'Content-Type': 'application/json',
+    Accept: 'application/json'
+};
+
+export async function getRequest(url){
+    const response = await axiosClient.get(`/${url}`);
+    return response;
+}
+
+export async function postRequst(url, payload){
+    const response = await axiosClient.post(`/${url}`, payload);
+    return response;
+}
+
+export async function patchRequest(url, payload){
+    const response = await axiosClient.patch(`/${url}`, payload);
+    return response;
+}
+
+export async function deleteRequest(){
+    const response = await axiosClient.delete(`/${url}`);
+    return response;
+}
