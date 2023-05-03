0d02daf746fe22583f1a76635357d5e6d268f24e
MlamliLolwane <mrlolwane96@gmail.com>
2023-05-04 01:03:44 +0200

    Delete learner

---
+++ src/components/ModalDeleteLearner.js
@@ -0,0 +1,85 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import axios from 'axios';
+import { useState } from 'react'
+
+function Modal({ learner }) {
+    const [active, setActive] = useState(false);
+
+    function DeleteLearner(id) {
+        setActive(true);
+        axios.delete('http://localhost:8000/learners/delete/' + id)
+            .then(() => {
+                setActive(false);
+            })
+    }
+
+    return (
+        <LoadingOverlay
+            active={active}
+            styles={{
+                overlay: (base) => ({
+                    ...base,
+                    background: '#1d1b1bf6',
+                    height: '100vh'
+                }),
+            }}
+            spinner={<HashLoader color="#4b9263" />}
+        >
+            <div>
+                <div className="modal fade" id="deleteModal" tabIndex="-1" aria-labelledby="modalDelete" aria-hidden="true">
+                    <div className="modal-dialog modal-dialog-centered">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title fw-bolder" id="exampleModalLabel">Confirm Delete</h5>
+                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div className="modal-body">
+                                <p className="text-center mb-0 pb-0">Are you sure you want to delete the following learner?</p> <br />
+                                <table className="table pt-0 mt-0">
+                                    <thead>
+                                        <tr>
+                                            <th> </th>
+                                            <th> </th>
+                                        </tr>
+                                    </thead>
+                                    {
+                                        <tbody>
+                                            <tr>
+                                                <td className="fw-light">Name</td>
+                                                <td className="fw-light">{learner.first_name + ' ' + learner.last_name}</td>
+                                            </tr>
+                                            <tr>
+                                                <td className="fw-light">Grade</td>
+                                                <td className="fw-light">{learner.grade}</td>
+                                            </tr>
+                                            <tr>
+                                                <td className="fw-light ">Cellphone Number</td>
+                                                <td className="fw-light ">{learner.cellphone}</td>
+                                            </tr>
+                                            <tr>
+                                                <td className="fw-light ">Whatsapp Number</td>
+                                                <td className="fw-light ">{learner.whatsapp}</td>
+                                            </tr>
+                                            <tr>
+                                                <td className="fw-light ">Email Address</td>
+                                                <td className="fw-light ">{learner.email}</td>
+                                            </tr>
+                                        </tbody>
+                                    }
+                                </table>
+
+                            </div>
+                            <div className="text-center pb-4 pt-1">
+                                <button type="submit" className="btn btn-primary me-2" onClick={() => { DeleteLearner(learner.id) }} data-bs-dismiss="modal">
+                                    DELETE <FontAwesomeIcon icon="fa-solid fa-delete-left" />
+                                </button>
+                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">CANCEL</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </LoadingOverlay>
+    )
+}
+export default Modal;
--- src/views/aggregated/LearnerInformation.js
+++ src/views/aggregated/LearnerInformation.js
@@ -7,10 +7,12 @@ import { getRequest } from '../../axiosClient';
 import axios from 'axios';
 import LoadingOverlay from 'react-loading-overlay-ts';
 import HashLoader from 'react-spinners/HashLoader';
+import Modal from '../../components/ModalDeleteLearner';
 
 function LearnerInformation() {
     const [learnerInfo, setLearnerInfo] = useState([]);
     const [active, setActive] = useState(true);
+    const [learner, setLearner] = useState('');
 
     useEffect(() => {
         axios.get('http://localhost:8000/api/query/learner-information')
@@ -20,69 +22,81 @@ function LearnerInformation() {
             })
     }, []);
 
+    function DeleteLearner(learner) {
+        setLearner(learner);
+    }
+
     return (
         <LoadingOverlay
-        active={active}
-        styles={{
-            overlay: (base) => ({
-                ...base,
-                background: '#1d1b1bf6',
-                height: '100vh'
-            }),
-        }}
-        spinner={<HashLoader color="#4b9263"/>}
+            active={active}
+            styles={{
+                overlay: (base) => ({
+                    ...base,
+                    background: '#1d1b1bf6',
+                    height: '100vh'
+                }),
+            }}
+            spinner={<HashLoader color="#4b9263" />}
         >
-        <div>
-            <NavbarSignedIn />
-            <div className="container">
-                <div className="row mt-5 pb-2">
-                    <div className="col">
-                        <h5 className="fw-bold d-inline">ALL LEARNERS</h5>
-                        <NavLink to="/learners/add" className="btn btn-sm btn-primary float-end">
-                            ADD LEARNER <FontAwesomeIcon icon="fa-solid fa-plus-circle" /> </NavLink>
+            <div>
+                <NavbarSignedIn />
+                <Modal learner={learner} />
+                <div className="container">
+                    <div className="row mt-5 pb-2">
+                        <div className="col">
+                            <h5 className="fw-bold d-inline">ALL LEARNERS</h5>
+                            <NavLink to="/learners/add" className="btn btn-sm btn-primary float-end">
+                                ADD LEARNER <FontAwesomeIcon icon="fa-solid fa-plus-circle" /> </NavLink>
+                        </div>
                     </div>
-                </div>
 
-                <div className="row mb-5">
-                    <div className="col">
-                        <div className="table-responsive">
-                            <table className="table table-dark">
-                                <thead>
-                                    <tr>
-                                        <th>NAME</th>
-                                        <th>GRADE</th>
-                                        <th>CELLPHONE</th>
-                                        <th>WHATSAPP</th>
-                                        <th>EMAIL</th>
-                                        <th>ACTION</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {
-                                        learnerInfo.map((learner) => {
-                                            return (
-                                                <tr key={learner.id}>
-                                                    <td className="fw-semibold">{
-                                                        learner.first_name
-                                                    } {learner.last_name}</td>
-                                                    <td className="fw-semibold">{learner.grade}</td>
-                                                    <td className="fw-semibold">{learner.cellphone}</td>
-                                                    <td className="fw-semibold">{learner.whatsapp}</td>
-                                                    <td className="fw-semibold">{learner.email}</td>
-                                                    <td><FontAwesomeIcon icon="fa-solid fa-ellipsis-vertical" />
-                                                    </td>
-                                                </tr>
-                                            )
-                                        })
-                                    }
-                                </tbody>
-                            </table>
+                    <div className="row mb-5">
+                        <div className="col">
+                            <div className="table-responsive">
+                                <table className="table table-dark">
+                                    <thead>
+                                        <tr>
+                                            <th>NAME</th>
+                                            <th>GRADE</th>
+                                            <th>CELLPHONE</th>
+                                            <th>WHATSAPP</th>
+                                            <th>EMAIL</th>
+                                            <th>ACTION</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {
+                                            learnerInfo.map((learner) => {
+                                                return (
+                                                    <tr key={learner.id}>
+                                                        <td className="fw-semibold">{
+                                                            learner.first_name
+                                                        } {learner.last_name}</td>
+                                                        <td className="fw-semibold">{learner.grade}</td>
+                                                        <td className="fw-semibold">{learner.cellphone}</td>
+                                                        <td className="fw-semibold">{learner.whatsapp}</td>
+                                                        <td className="fw-semibold">{learner.email}</td>
+                                                        <td><div className="text-center">
+                                                            <button type="button" className="btn btn-sm btn-primary me-2">
+                                                                UPDATE <FontAwesomeIcon icon="fa-solid fa-pencil-alt" />
+                                                            </button>
+                                                            <button onClick={() => DeleteLearner(learner)} className="btn btn-sm btn-secondary" data-bs-toggle="modal" data-bs-target="#deleteModal">
+                                                                DELETE <FontAwesomeIcon icon="fa-solid fa-delete-left" />
+                                                            </button>
+                                                        </div>
+                                                        </td>
+                                                    </tr>
+                                                )
+                                            })
+                                        }
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
+                <Footer />
             </div>
-            <Footer />
-        </div>
         </LoadingOverlay>
     )
 }
